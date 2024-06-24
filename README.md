e3eaddfa507cc5fca86482922ae5f59bf3428adf
Fredrik Sundberg <ted.sundberg@gmail.com>
2024-06-24 21:44:30 +0200

    Initial commit - Vite starting point

---
+++ README.md
@@ -0,0 +1,3 @@
+# Domain-finder
+
+Tool for generating suggestions for domain names, as well as checking their avilability.
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Domain dinder</h1>
+    </>
+  );
+}
+
+export default App;
