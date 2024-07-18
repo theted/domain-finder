69a52b0e76b966acd039977365507541b483ded0
Fredrik Sundberg <ted.sundberg@gmail.com>
2024-07-18 02:39:13 +0200

    Fix incorrect import path

--- backend/server.ts
+++ backend/server.ts
@@ -4,9 +4,13 @@ import bodyParser from "body-parser";
 import { Server } from "socket.io";
 import { QueueEvents } from "bullmq";
 import { Queue } from "bullmq";
-import { QUQUE_OPTIONS } from "./config";
+import { QUQUE_OPTIONS } from "./config.ts";
 import path from "path";
+import { fileURLToPath } from "url";
 import { fork } from "child_process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = 5000;
 const app = express();
@@ -57,6 +61,10 @@ queueEvents.on("completed", (data) => {
   console.log("check domain event", data);
   io.emit("check-domain", data);
 });
+
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = "development";
+}
 
 // if in development, also spin up a worker as a child process
 if (process.env.NODE_ENV === "development") {
