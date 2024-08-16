1565c67b9243d5388e6838224aec39e20444448b
Fredrik Sundberg <ted.sundberg@gmail.com>
2024-08-16 02:00:03 +0200

    Initial AWS EC2 deploy in CI/CD

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -2,12 +2,20 @@ import { beforeAll, afterEach, vi } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 
+const ResizeObserverMock = vi.fn(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
+
 beforeAll(() => {
   // mock pointer events
   window.PointerEvent = class PointerEvent extends Event {};
   window.HTMLElement.prototype.scrollIntoView = vi.fn();
   window.HTMLElement.prototype.hasPointerCapture = vi.fn();
   window.HTMLElement.prototype.releasePointerCapture = vi.fn();
+
+  vi.stubGlobal("ResizeObserver", ResizeObserverMock);
 });
 
 // runs a clean after each test case (e.g. clearing jsdom)
